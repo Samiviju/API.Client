6baf93fd3143d2d3db9870cf82c166b63f4f1a08
samuel.vieira <samuel.vieira@bit.com.br>
2022-03-08 18:10:33 -0300

    Primeiro commit da API para o repositorio

---
+++ ClientsAPI/Model/Client.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ClientsAPI.Model
+{
+    public class Client
+    {
+        public string Cpf { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string LastName  { get; set; }
+    }
+}
---
+++ ClientsAPI/Repositories/Entities/IClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using ClientsAPI.Model;
+
+namespace ClientsAPI.Repositories
+{
+    public interface IClientRepository
+    {
+        Task<IEnumerable<Client>> Get();
+        Task<Client> Get(string cpf);
+        Task<Client> Create(Client client);
+        Task Update(Client client);
+        Task Delete(string cpf);
+    }
+}
